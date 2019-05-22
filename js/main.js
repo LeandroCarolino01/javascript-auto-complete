d5cfc0e0d01b2e2521387c2a83c8e19450664266
Leandro.Carolino <carolino.leandro@gmail.com>
2019-05-22 18:20:11 +0200

    layout added

--- js/main.js
+++ js/main.js
@@ -6,6 +6,32 @@ const searchStates = async searchText => {
     const res = await fetch('../data/state.json');
     const states = await res.json();
 
-    console.log(states);
+    //console.log(states);
+    //get matches to current text input
+    let matches = states.filter(state => {
+        const regex = new RegExp(`${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    });
+    if ( searchText.length === 0){
+        matches = [];
+    }
+
+    outPutHtml(matches);
+
 }
+
+const outPutHtml = matches => {
+    if(matches.length > 0){
+        const html = matches.map(match => `
+            <div class="card card-body mb-1">
+                <h4>${match.name}(${match.abbr}) <span class="text-primary">${match.capital}</span></h4>
+                <small>Lat: ${match.lat} / Long: ${match.long}</small>
+            </div>
+        `)
+        .join('')
+
+        matchList.innerHTML = html;
+    }
+}
+
 search.addEventListener('input', () => searchStates(search.value));
